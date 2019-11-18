5e2bcbe19efcb8fd63a1669b969847790245d2c2
John Knox <jgk305@nyu.edu>
2019-11-18 09:53:20 -0500

    init lozad and glide

---
+++ library-javascript.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  /// Init GLIDE for carousel slideshow
+  new Glide('.glide').mount()
+
+  /// Init LOZAD for lazy loading
+  const observer = lozad('.lozad', {
+    loaded: function(el) {
+      el.classList.add('lozad-fully-loaded');
+    }
+  });
+  observer.observe();
+
+});
